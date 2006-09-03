7875444a0ffe44877c3bf846c3d9fcd78b783c4d
(no author) <(no author)@7371544b-871c-0410-9858-fa51e0ccc7c8>
2006-09-03 07:53:56 +0000

    Created initial request controller code

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,2 +1,33 @@
 class RequestController < ApplicationController
+
+  def index
+    list
+    render :action => "list"
+  end
+  
+  def list
+    
+    @requests = Request.find(:all, :conditions => "purchase_id = 0", :order => "created_at ASC")
+    
+  end
+  
+  def new
+  
+    @request = Request.new
+  
+  end
+  
+  def create
+  
+    @request = Requests.new params[:request]
+    
+    if @request.valid?
+      @request.save
+      redirect_to :action => "list"
+    else
+      render :action => "new"
+    end
+  
+  end
+  
 end
