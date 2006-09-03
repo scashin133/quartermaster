94e6f5787f189690caf8178aaf8cd5c7d7c38232
(no author) <(no author)@7371544b-871c-0410-9858-fa51e0ccc7c8>
2006-09-03 10:58:44 +0000

    Got autocomplete working

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,8 +1,11 @@
 class ItemController < ApplicationController
 
+  layout false
+
   def auto_complete_for_item_name
     @items = Item.find(:all,
       :conditions => [ 'LOWER(name) LIKE ?',
-      '%' + params[:name].downcase + '%' ])
+      params[:new_request][:item].downcase + '%' ])
+    render :inline => "<%= auto_complete_result(@items, 'name') %>"
   end
 end
